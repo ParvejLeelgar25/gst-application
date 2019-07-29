36603bf0493dc6feacbf6acad05c254471a59870
parvejleelgar <parvejchauhan23@gmail.com>
2019-07-29 17:51:34 +0530

    set dynamic path for image

--- modules/axelor-gst/src/main/java/com/axelor/gst/web/ProductController.java
+++ modules/axelor-gst/src/main/java/com/axelor/gst/web/ProductController.java
@@ -3,6 +3,7 @@ package com.axelor.gst.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.axelor.app.AppSettings;
 import com.axelor.gst.db.Invoice;
 import com.axelor.gst.db.InvoiceLine;
 import com.axelor.gst.db.Party;
@@ -15,8 +16,6 @@ import com.google.inject.Inject;
 
 public class ProductController {
 
-  
-
   public void report(ActionRequest request, ActionResponse response) {
 
     List<Integer> ids;
@@ -26,6 +25,9 @@ public class ProductController {
       String ids_str = ids.toString();
       String productId = ids_str.substring(1, ids_str.length() - 1);
       request.getContext().put("productId", productId);
+      String imagePath = AppSettings.get().getPath("file.upload.dir", "file.upload.dir");
+      System.out.println(imagePath);
+      request.getContext().put("imagePath", imagePath);
     }
   }
 
@@ -35,16 +37,21 @@ public class ProductController {
     if ((ids = (List) request.getContext().get("productIds")) == null) {
       throw new IllegalArgumentException("Please Select Atleast One Record");
     } else {
-    	Party party = (Party) request.getContext().get("party");
-    	Long partyId = party.getId();
-    	request.getContext().put("partyId", partyId);
-    	response.setView(
-    	          ActionView.define("Invoice")
-    	              .model(Invoice.class.getName())
-    	              .add("form", "invoice-form")
-    	              .context("product_ids", request.getContext().get("productIds"))
-    	              .context("party_id", request.getContext().get("partyId"))
-    	              .map());
+      if (request.getContext().get("party") != null) {
+        Party party = (Party) request.getContext().get("party");
+        Long partyId = party.getId();
+        request.getContext().put("partyId", partyId);
+        response.setView(
+            ActionView.define("Invoice")
+                .model(Invoice.class.getName())
+                .add("form", "invoice-form")
+                .context("product_ids", request.getContext().get("productIds"))
+                .context("party_id", request.getContext().get("partyId"))
+                .map());
+      }
+      else {
+    	  response.setError("Please select Party");
+      }
     }
     response.setCanClose(true);
   }
